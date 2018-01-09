c90aa3d3c5916db175654c1f0ceda2b8eb32eb81
李永磊 <liylo@chanjet.com>
2018-01-09 17:08:51 +0800

    Can write the generated JSON into the target file

--- README.md
+++ README.md
@@ -33,4 +33,7 @@ Emits `html-manifest.json` as file in the output directory
 |:--:|:--:|:-----:|:----------| 
 |path|string|undefined|File path|
 |filename|string|html-manifest.json|File name|
+|templates|string or array|undefined|The generated JSON will inject the template files (the template file is the template in the html-webpack-plugin)|
+|variable|string|html_manifest_json|JSON variable name injected into the template files|
+
 
--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var fs = require("fs");
 
 function HtmlManifestPlugin(options) {
     this.options = Object.assign({},{
-        filename:"html-manifest.json"
+        filename:"html-manifest.json",
+        variable:"html_manifest_json"
     },options);
     this.content={};
 }
@@ -21,6 +22,33 @@ HtmlManifestPlugin.prototype.apply = function(compiler) {
             var outputPath = path.isAbsolute(this.options.path||"")?
                 this.options.path:
                 path.join(compiler.options.output.path,this.options.path||"");
+            var targetTemplates=[];
+            if(typeof this.options.templates === "string"){
+                targetTemplates.push(this.options.templates);
+            }else if(this.options.templates instanceof Array){
+                targetTemplates = this.options.templates;
+            };debugger;
+            targetTemplates.forEach((template)=>{
+                let filename = this.content[path.basename(template)];
+                let insertStr = `<script>var ${this.options.variable} = ${JSON.stringify(this.content)}</script>`;
+                fs.readFile(path.join(outputPath,filename), 'utf8', (err,data)=>{
+                    if(err)throw err;
+                    if(/\<script/.test(data)){
+                        data = data.replace(/\<script/,(match,offset)=>{
+                            return insertStr+match;
+                        });
+                    }else if(/\<\/head/.test(data)){
+                        data = data.replace(/\<\/head/,(match,offset)=>{
+                            return insertStr+match;
+                        });
+                    }else{
+                        data = insertStr+data;
+                    }
+                    fs.writeFile(path.join(outputPath,filename),data, (err) => {
+                        if (err) throw err;
+                    });
+                });
+            });
             if(!fs.existsSync(outputPath)){
                 fs.mkdir(outputPath,(err)=>{
                     if(err)throw err;
